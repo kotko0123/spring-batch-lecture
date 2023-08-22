ccdbf30d50021559ea9b09292aadabbf84b92122
kim_jinyoung_aart <kotko123@naver.com>
2023-08-23 00:36:26 +0900

    stepContribution, executionContext 추가

--- src/main/java/io/springbatch/springbatchlecture/config/stepContribution/StepContributionConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/config/stepContribution/StepContributionConfiguration.java
@@ -1,11 +1,14 @@
-//package io.springbatch.springbatchlecture.config.stepExecution;
+//package io.springbatch.springbatchlecture.config.stepContribution;
 //
 //import lombok.RequiredArgsConstructor;
 //import lombok.extern.slf4j.Slf4j;
 //import org.springframework.batch.core.Job;
 //import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.StepContribution;
 //import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 //import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
 //import org.springframework.batch.repeat.RepeatStatus;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
@@ -13,46 +16,38 @@
 //@Slf4j
 //@Configuration
 //@RequiredArgsConstructor
-//public class StepExecutionConfiguration {
+//public class StepContributionConfiguration {
 //
 //  private final JobBuilderFactory jobBuilderFactory;
 //  private final StepBuilderFactory stepBuilderFactory;
 //
 //  @Bean
 //  public Job job() {
-//    return jobBuilderFactory.get("job-StepExecution1")
+//    return jobBuilderFactory.get("job-StepContribution")
 //      .start(step1())
 //      .next(step2())
-//      .next(step3())
 //      .build();
 //  }
 //
 //  @Bean
 //  public Step step1() {
-//    return stepBuilderFactory.get("step1-StepExecution")
-//      .tasklet((contribution, chunkContext) -> {
-//        log.info("### step1-StepExecution");
-//        return RepeatStatus.FINISHED;
+//    return stepBuilderFactory.get("step1-StepContribution")
+//      .tasklet(new Tasklet() {
+//        @Override
+//        public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//          log.info("### step1-StepContribution");
+//          System.out.println(contribution);
+//          return RepeatStatus.FINISHED;
+//        }
 //      })
 //      .build();
 //  }
 //
 //  @Bean
 //  public Step step2() {
-//    return stepBuilderFactory.get("step2-StepExecution")
+//    return stepBuilderFactory.get("step2-StepContribution")
 //      .tasklet((contribution, chunkContext) -> {
-//        log.info("### step2-StepExecution");
-////        throw new RuntimeException("test error");
-//        return RepeatStatus.FINISHED;
-//      })
-//      .build();
-//  }
-//
-//  @Bean
-//  public Step step3() {
-//    return stepBuilderFactory.get("step3-StepExecution")
-//      .tasklet((contribution, chunkContext) -> {
-//        log.info("### step3-StepExecution");
+//        log.info("### step2-StepContribution");
 //        return RepeatStatus.FINISHED;
 //      })
 //      .build();
