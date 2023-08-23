dc02e2f8b11e192d1e12159afdf4ea0b8ddbf432
kim_jinyoung_aart <kotko123@naver.com>
2023-08-23 23:40:10 +0900

    jobLauncher 추가

--- src/main/java/io/springbatch/springbatchlecture/config/jobRepository/JobRepositoryConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/config/jobRepository/JobRepositoryConfiguration.java
@@ -1,50 +1,50 @@
-package io.springbatch.springbatchlecture.config.jobRepository;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Slf4j
-@Configuration
-@RequiredArgsConstructor
-public class JobRepositoryConfiguration {
-
-  private final JobBuilderFactory jobBuilderFactory;
-  private final StepBuilderFactory stepBuilderFactory;
-  private final JobRepositoryListener jobRepositoryListener;
-
-  @Bean
-  public Job job() {
-    return jobBuilderFactory.get("batchJob")
-      .start(step1())
-      .next(step2())
-      .listener(jobRepositoryListener)
-      .build();
-  }
-
-  @Bean
-  public Step step1() {
-    return stepBuilderFactory.get("step1-JobRepository")
-      .tasklet((contribution, chunkContext) -> {
-        log.info("### step1-JobRepository");
-        return RepeatStatus.FINISHED;
-      })
-      .build();
-  }
-
-  @Bean
-  public Step step2() {
-    return stepBuilderFactory.get("step2-JobRepository")
-      .tasklet((contribution, chunkContext) -> {
-        log.info("### step2-JobRepository");
-        return RepeatStatus.FINISHED;
-      })
-      .build();
-  }
-}
+//package io.springbatch.springbatchlecture.config.jobRepository;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Slf4j
+//@Configuration
+//@RequiredArgsConstructor
+//public class JobRepositoryConfiguration {
+//
+//  private final JobBuilderFactory jobBuilderFactory;
+//  private final StepBuilderFactory stepBuilderFactory;
+//  private final JobRepositoryListener jobRepositoryListener;
+//
+//  @Bean
+//  public Job job() {
+//    return jobBuilderFactory.get("batchJob")
+//      .start(step1())
+//      .next(step2())
+//      .listener(jobRepositoryListener)
+//      .build();
+//  }
+//
+//  @Bean
+//  public Step step1() {
+//    return stepBuilderFactory.get("step1-JobRepository")
+//      .tasklet((contribution, chunkContext) -> {
+//        log.info("### step1-JobRepository");
+//        return RepeatStatus.FINISHED;
+//      })
+//      .build();
+//  }
+//
+//  @Bean
+//  public Step step2() {
+//    return stepBuilderFactory.get("step2-JobRepository")
+//      .tasklet((contribution, chunkContext) -> {
+//        log.info("### step2-JobRepository");
+//        return RepeatStatus.FINISHED;
+//      })
+//      .build();
+//  }
+//}
--- src/main/java/io/springbatch/springbatchlecture/config/jobRepository/JobRepositoryListener.java
+++ src/main/java/io/springbatch/springbatchlecture/config/jobRepository/JobRepositoryListener.java
@@ -1,42 +1,42 @@
-package io.springbatch.springbatchlecture.config.jobRepository;
-
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class JobRepositoryListener implements JobExecutionListener {
-
-  @Autowired
-  private JobRepository jobRepository;
-
-  @Override
-  public void beforeJob(JobExecution jobExecution) {
-
-  }
-
-  @Override
-  public void afterJob(JobExecution jobExecution) {
-    String jobName = jobExecution.getJobInstance().getJobName();
-
-    JobParameters jobParameters = new JobParametersBuilder()
-      .addString("requestDate", "20210102")
-      .toJobParameters();
-
-    JobExecution lastJobExecution = jobRepository.getLastJobExecution(jobName, jobParameters);
-
-    if (lastJobExecution != null) {
-      JobParameters jobParameters1 = lastJobExecution.getJobParameters();
-      System.out.println("jobParameters1 ::: " + jobParameters1);
-      for (StepExecution stepExecution : lastJobExecution.getStepExecutions()) {
-        BatchStatus status = stepExecution.getStatus();
-        ExitStatus exitStatus = stepExecution.getExitStatus();
-        String stepName = stepExecution.getStepName();
-        System.out.println("status : " + status);
-        System.out.println("exitStatus : " + exitStatus);
-        System.out.println("stepName : " + stepName);
-      }
-    }
-  }
-}
+//package io.springbatch.springbatchlecture.config.jobRepository;
+//
+//import org.springframework.batch.core.*;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class JobRepositoryListener implements JobExecutionListener {
+//
+//  @Autowired
+//  private JobRepository jobRepository;
+//
+//  @Override
+//  public void beforeJob(JobExecution jobExecution) {
+//
+//  }
+//
+//  @Override
+//  public void afterJob(JobExecution jobExecution) {
+//    String jobName = jobExecution.getJobInstance().getJobName();
+//
+//    JobParameters jobParameters = new JobParametersBuilder()
+//      .addString("requestDate", "20210102")
+//      .toJobParameters();
+//
+//    JobExecution lastJobExecution = jobRepository.getLastJobExecution(jobName, jobParameters);
+//
+//    if (lastJobExecution != null) {
+//      JobParameters jobParameters1 = lastJobExecution.getJobParameters();
+//      System.out.println("jobParameters1 ::: " + jobParameters1);
+//      for (StepExecution stepExecution : lastJobExecution.getStepExecutions()) {
+//        BatchStatus status = stepExecution.getStatus();
+//        ExitStatus exitStatus = stepExecution.getExitStatus();
+//        String stepName = stepExecution.getStepName();
+//        System.out.println("status : " + status);
+//        System.out.println("exitStatus : " + exitStatus);
+//        System.out.println("stepName : " + stepName);
+//      }
+//    }
+//  }
+//}
